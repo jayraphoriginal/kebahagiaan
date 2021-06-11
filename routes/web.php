c21f6abd454a15bfb70c397076938d63013165db
jayraph <rafel101990@gmail.com>
2021-06-11 22:34:42 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-    // Route::view('forms', 'forms')->name('forms');
-    // Route::view('cards', 'cards')->name('cards');
-    // Route::view('charts', 'charts')->name('charts');
-    // Route::view('buttons', 'buttons')->name('buttons');
-    // Route::view('modals', 'modals')->name('modals');
-    // Route::view('tables', 'tables')->name('tables');
-    // Route::view('calendar', 'calendar')->name('calendar');
+    Route::view('forms', 'forms')->name('forms');
+    Route::view('cards', 'cards')->name('cards');
+    Route::view('charts', 'charts')->name('charts');
+    Route::view('buttons', 'buttons')->name('buttons');
+    Route::view('modals', 'modals')->name('modals');
+    Route::view('tables', 'tables')->name('tables');
+    Route::view('calendar', 'calendar')->name('calendar');
 
 });
