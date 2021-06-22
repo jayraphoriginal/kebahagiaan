4e755277d827a62a141a223b8657761794edcc6e
jayraph <rafel101990@gmail.com>
2021-06-22 16:39:26 +0700

    edit laporan

--- resources/views/livewire/laporan.blade.php
+++ resources/views/livewire/laporan.blade.php
@@ -28,7 +28,7 @@
                 class="flex-1/2 w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"/>
             </div>
 
-            <h4>Total Penjualan  : {{  }}</h4>
+            <h4>Total Penjualan  : {{ number_format($total,0,',','.') }}</h4>
 
             <div>
                 <table class="w-full whitespace-no-wrap">
